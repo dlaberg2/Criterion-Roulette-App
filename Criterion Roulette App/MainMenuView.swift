1f5408091e8a0e90234a019c56d46445012b7849
Dylan <dylan3665@gmail.com>
2026-03-05 14:16:46 -0500

    Add Images and start building backgrounds and buttons MainMenuView

---
+++ Criterion Roulette App/MainMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuView.swift
+//  Criterion Roulette App
+//
+//  Created by Dylan Laberge on 3/5/26.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenuView()
+}
---
+++ Criterion Roulette App/SessionView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionView.swift
+//  Criterion Roulette App
+//
+//  Created by Dylan Laberge on 3/5/26.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionView()
+}
